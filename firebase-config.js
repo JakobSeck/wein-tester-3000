5b5c5259bc440c2d0781893ad691d413f2cec00b
JakobSeck <jakob.seck@gmail.com>
2025-05-21 00:08:53 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyASD7sd9H8zC4eWQG4hXhu7OlKZlZbgxTo",
+  authDomain: "weintester-6e16b.firebaseapp.com",
+  projectId: "weintester-6e16b",
+  storageBucket: "weintester-6e16b.firebasestorage.app",
+  messagingSenderId: "246274264172",
+  appId: "1:246274264172:web:c417c43f66d81d68fc1c49",
+  measurementId: "G-6DG76XNGG8"
+};
